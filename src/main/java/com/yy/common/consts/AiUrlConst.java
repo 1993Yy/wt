ab73e08a4dd12163fab762cbed1764a94d0e5e51
Yy <940357154@qq.com>
2020-09-23 23:24:19 +0800

    11

---
+++ src/main/java/com/yy/common/consts/AiUrlConst.java
@@ -0,0 +1,52 @@
+package com.yy.common.consts;
+
+/**
+ * @package: com.yy.common
+ * @className: AiUrlConst
+ * @author: Created By Yy
+ * @date: 2020-09-23 22:51
+ */
+public class AiUrlConst {
+
+    public static final String orc_idCard="https://api.ai.qq.com/fcgi-bin/ocr/ocr_idcardocr";
+    public static final String orc_driverLicense="https://api.ai.qq.com/fcgi-bin/ocr/ocr_driverlicenseocr";
+    public static final String orc_general="https://api.ai.qq.com/fcgi-bin/ocr/ocr_generalocr";
+    public static final String orc_bizLicense="https://api.ai.qq.com/fcgi-bin/ocr/ocr_bizlicenseocr";
+    public static final String orc_creditCard="https://api.ai.qq.com/fcgi-bin/ocr/ocr_creditcardocr";
+    public static final String orc_handWriting="https://api.ai.qq.com/fcgi-bin/ocr/ocr_handwritingocr";
+    public static final String orc_plate="https://api.ai.qq.com/fcgi-bin/ocr/ocr_plateocr";
+    public static final String orc_bc="https://api.ai.qq.com/fcgi-bin/ocr/ocr_bcocr";
+
+    public static final String face_detect="https://api.ai.qq.com/fcgi-bin/face/face_detectface";
+    public static final String face_detectMulti="https://api.ai.qq.com/fcgi-bin/face/face_detectmultiface";
+    public static final String face_detectcrossage="https://api.ai.qq.com/fcgi-bin/face/face_detectcrossageface";
+    public static final String face_faceshape="https://api.ai.qq.com/fcgi-bin/face/face_faceshape";
+    public static final String face_facecompare="https://api.ai.qq.com/fcgi-bin/face/face_facecompare";
+    public static final String face_faceverify="https://api.ai.qq.com/fcgi-bin/face/face_faceverify";
+
+    public static final String ptu_imgfilter="https://api.ai.qq.com/fcgi-bin/ptu/ptu_imgfilter";
+    public static final String vision_imgfilter="https://api.ai.qq.com/fcgi-bin/vision/vision_imgfilter";
+    public static final String ptu_facecosmetic="https://api.ai.qq.com/fcgi-bin/ptu/ptu_facecosmetic";
+    public static final String ptu_facedecoration="https://api.ai.qq.com/fcgi-bin/ptu/ptu_facedecoration";
+    public static final String ptu_facesticker="https://api.ai.qq.com/fcgi-bin/ptu/ptu_facesticker";
+    public static final String ptu_faceage="https://api.ai.qq.com/fcgi-bin/ptu/ptu_faceage";
+
+    public static final String vision_imgtotext="https://api.ai.qq.com/fcgi-bin/vision/vision_imgtotext";
+    public static final String image_tag="https://api.ai.qq.com/fcgi-bin/image/image_tag";
+    public static final String image_fuzzy="https://api.ai.qq.com/fcgi-bin/image/image_fuzzy";
+    public static final String image_food="https://api.ai.qq.com/fcgi-bin/image/image_food";
+
+    public static final String image_terrorism="https://api.ai.qq.com/fcgi-bin/image/image_terrorism";
+    public static final String vision_porn="https://api.ai.qq.com/fcgi-bin/vision/vision_porn";
+    public static final String aai_evilaudio="https://api.ai.qq.com/fcgi-bin/aai/aai_evilaudio";
+
+    public static final String nlp_textchat="https://api.ai.qq.com/fcgi-bin/nlp/nlp_textchat";
+
+    public static final String nlp_texttranslate="https://api.ai.qq.com/fcgi-bin/nlp/nlp_texttranslate";
+    public static final String nlp_speechtranslate="https://api.ai.qq.com/fcgi-bin/nlp/nlp_speechtranslate";
+    public static final String nlp_imagetranslate="https://api.ai.qq.com/fcgi-bin/nlp/nlp_imagetranslate";
+    public static final String nlp_textdetect="https://api.ai.qq.com/fcgi-bin/nlp/nlp_textdetect";
+
+    public static final String nlp_wordcom="https://api.ai.qq.com/fcgi-bin/nlp/nlp_wordcom";
+
+}
